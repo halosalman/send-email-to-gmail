b494ced10dae6c365a0af2cb261e4313fd173267
Halo Salman <43407295+halosalman@users.noreply.github.com>
2022-06-14 10:46:32 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -6,10 +6,10 @@ $message= $_POST['message'];
 $to = "halosalman@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com";
+$headers = "From: noreply@Home4T.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect
-header("Location:https://raw.githack.com/halosalman/send-email-to-gmail/main/thankyou.html");
+header("Location:https://yt.home4t.com/");
 ?>
